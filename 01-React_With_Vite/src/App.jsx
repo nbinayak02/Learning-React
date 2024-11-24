cf1920499dfb211901f3b14b1486a964b58b3810
binayak-2 <binayakniraula2@gmail.com>
2024-11-24 20:31:07 +0545

    Add variables in html element in jsx

--- 01-React_With_Vite/src/App.jsx
+++ 01-React_With_Vite/src/App.jsx
@@ -1,24 +1,33 @@
 import Hello from './Hello'
 function App() {
-  
+  const date = new Date();
+  const fullyear = date.getFullYear();
 
   return (
+
     // we cannot return multiple elements like this 
-  //  <h1>Hello World! React</h1>
-  //   <Hello />
+    //  <h1>Hello World! React</h1>
+    //   <Hello />
 
-  // we can return by surrounding with div 
-  // <div>
-  //   <h1>Hello World! -React</h1>
-  //   <Hello/>
-  // </div>
+    // we can return by surrounding with div 
+    // <div>
+    //   <h1>Hello World! -React</h1>
+    //   <Hello/>
+    // </div>
 
-  // to make it simple we can just do like this 
-  <>
-  <h1>Hello World! -React</h1>
-  <Hello/> 
-  {/* this is the function call to function Hello */}
-  </>
+    // to make it simple we can just do like this 
+
+    <>
+
+      <h1>Hello World! -React {fullyear}</h1>
+
+      {/* to access variable we use {variableName} as above but we cannot write js code inside that braces, only evaulated expression is allowed meaning that only variable name is allowed and that variable should hold the output. Why js code not supported? because at the end jsx gets parsed into js object and js object doesn't support js code as its property, it can only support variable */}
+
+      <Hello />
+
+      {/* above is the function call to function Hello */}
+
+    </>
   )
 }
 
--- 01-React_With_Vite/src/main.jsx
+++ 01-React_With_Vite/src/main.jsx
@@ -4,6 +4,8 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    {/* this is the function call to App(). The html inside that function gets converted into object then only it is rendered in screen. */}
     <App />
+
   </StrictMode>,
 )
